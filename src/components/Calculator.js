5ba0fcc7f09661108407dc20e66a795c8b092fa4
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2022-11-29 23:46:15 +0500

    Create a component class

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,32 +1,37 @@
-function App() {
-  return (
-    <div className="App">
-      <div className="calculator">
-        <div className="display">0</div>
-        <div className="inputs">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button">/</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button">x</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button">-</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button">+</button>
-          <button type="button">0</button>
-          <button type="button">.</button>
-          <button type="button">=</button>
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="calculator">
+          <div className="display">0</div>
+          <div className="inputs">
+            <button type="button">AC</button>
+            <button type="button">+/-</button>
+            <button type="button">%</button>
+            <button type="button">/</button>
+            <button type="button">7</button>
+            <button type="button">8</button>
+            <button type="button">9</button>
+            <button type="button">x</button>
+            <button type="button">4</button>
+            <button type="button">5</button>
+            <button type="button">6</button>
+            <button type="button">-</button>
+            <button type="button">1</button>
+            <button type="button">2</button>
+            <button type="button">3</button>
+            <button type="button">+</button>
+            <button type="button">0</button>
+            <button type="button">.</button>
+            <button type="button">=</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+export default Calculator;
