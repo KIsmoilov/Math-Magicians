c1315de145e2cc3077693252eea1f45ad7f9e55b
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2022-11-30 12:37:27 +0500

    Fix the ESlint error per the request of the code reviewer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="App">
